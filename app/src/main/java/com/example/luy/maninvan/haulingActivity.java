d65c25de40d0d78a8917779f28b2011a93431106
yl89757jj <yanglu930309@gmail.com>
2017-11-23 06:48:36 -0500

    Send SMS
    +1 point for 3rd party API integration (e.g. sending SMS messages or
    Push Notifications)

--- app/src/main/java/com/example/luy/maninvan/haulingActivity.java
+++ app/src/main/java/com/example/luy/maninvan/haulingActivity.java
@@ -1,19 +1,31 @@
 package com.example.luy.maninvan;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class HaulingActivity extends AppCompatActivity {
+    String phoneNumber = "9177567156";
+    String message = "Hi Luna, thanks for accepting my request...";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hauling);
 
+        String name = "User";
+        TextView mock = (TextView) findViewById(R.id.van_note);
+        mock.setText(name + "Accept");
+    }
 
-        TextView mock = (TextView) findViewById(R.id.mock);
-        mock.setText("Mock Info");
+    public void sendMessage(View view){
+        //SMS APP will handle
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber));
+        intent.putExtra("sms_body", message);
+        startActivity(intent);
     }
 }
